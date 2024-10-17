3eb3a9dbe5d472dbeb71c8cc49909e7f2929a5e6
Your Name <Abdulrahman956>
2024-10-17 15:42:12 +0300

    add gesterDetector to item

--- lib/feature/home/presentation/views/widgets/best_seller_item.dart
+++ lib/feature/home/presentation/views/widgets/best_seller_item.dart
@@ -1,5 +1,7 @@
+import 'package:bookly_app/core/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/styles.dart';
@@ -13,78 +15,83 @@ class BestSellerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: SizedBox(
-        height: 125,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.8 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(AppAssets.testImage),
+      child: GestureDetector(
+        onTap: () {
+          GoRouter.of(context).push(AppRoutes.kDetailsView);
+        },
+        child: SizedBox(
+          height: 125,
+          child: Row(
+            children: [
+              AspectRatio(
+                aspectRatio: 2.8 / 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(AppAssets.testImage),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 30,
-            ),
-            const Expanded(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 200,
-                      child: Text(
-                        'Book Title Book Title',
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: AppStyles.titleMedium,
+              const SizedBox(
+                width: 30,
+              ),
+              const Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 200,
+                        child: Text(
+                          'Book Title Book Title',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: AppStyles.titleMedium,
+                        ),
                       ),
-                    ),
-                    Spacer(),
-                    Text(
-                      'Book Author',
-                      style: AppStyles.titleAuthor,
-                    ),
-                    Spacer(),
-                    Row(
-                      children: [
-                        Text(
-                          r'19.9 $',
-                          style: AppStyles.priceStyle,
-                        ),
-                        Spacer(),
-                        Icon(
-                          FontAwesomeIcons.solidStar,
-                          color: Colors.yellow,
-                          size: 20,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          '4.8',
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          r'(199)',
-                          style: AppStyles.titleAuthor,
-                        ),
-                      ],
-                    ),
-                  ],
+                      Spacer(),
+                      Text(
+                        'Book Author',
+                        style: AppStyles.titleAuthor,
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Text(
+                            r'19.9 $',
+                            style: AppStyles.priceStyle,
+                          ),
+                          Spacer(),
+                          Icon(
+                            FontAwesomeIcons.solidStar,
+                            color: Colors.yellow,
+                            size: 20,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            '4.8',
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            r'(199)',
+                            style: AppStyles.titleAuthor,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
