4db2a8bcde510a869957a4def73ec8320f928212
Your Name <Abdulrahman956>
2024-10-17 15:42:33 +0300

    add routes

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -1,8 +1,11 @@
+import 'package:bookly_app/feature/home/presentation/views/book_detials_view.dart';
 import 'package:bookly_app/feature/home/presentation/views/home_view.dart';
 import 'package:bookly_app/feature/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRoutes {
+  static const kHomeView = '/homview';
+  static const kDetailsView = '/detailsiew';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -10,8 +13,12 @@ abstract class AppRoutes {
         builder: (context, state) => const SplashView(),
       ),
       GoRoute(
-        path: '/homview',
+        path: AppRoutes.kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: AppRoutes.kDetailsView,
+        builder: (context, state) => const BookDetailsView(),
       ),
     ],
   );
