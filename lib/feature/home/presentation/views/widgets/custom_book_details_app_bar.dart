b8cf3b106745ba0f77205aad26382bbd6676c6b4
Your Name <Abdulrahman956>
2024-10-17 15:43:29 +0300

    create custom app bar of book details view

---
+++ lib/feature/home/presentation/views/widgets/custom_book_details_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomBookDetailsAppBar extends StatelessWidget {
+  const CustomBookDetailsAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20, top: 40, left: 20, right: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.close),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          )
+        ],
+      ),
+    );
+  }
+}
