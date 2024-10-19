933e0a348bda0a2078f400cc71fc65f6bb7a7038
Your Name <Abdulrahman956>
2024-10-19 14:49:03 +0300

    book detiaks view body donee

--- lib/feature/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/feature/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,15 +1,81 @@
+import 'package:bookly_app/core/widgets/books_actions.dart';
+import 'package:bookly_app/feature/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
+import 'book_rating.dart';
 import 'custom_book_details_app_bar.dart';
+import 'semiler_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    var width = MediaQuery.of(context).size.width;
+    return Column(
       children: [
-        CustomBookDetailsAppBar(),
+        const CustomBookDetailsAppBar(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: width * .5 / 2,
+          ),
+          child: const CustomBooksItem(),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const Text(
+          'Visual Essay On a Novel',
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          'Taylor Cooper',
+          style: TextStyle(
+            fontSize: 20,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const BookAction(),
+        const SizedBox(
+          height: 30,
+        ),
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 30),
+            child: Text(
+              'You can also see',
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const SemilerBooksLIstView(),
       ],
     );
   }
