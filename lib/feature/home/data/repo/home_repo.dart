ce5662a0fe45942f0a46603a1a271c520852f529
Your Name <Abdulrahman956>
2024-10-22 16:53:57 +0300

    handle server erroe

--- lib/feature/home/data/repo/home_repo.dart
+++ lib/feature/home/data/repo/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<AppFailure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<AppFailure, List<BookModel>>> fetchNewestBooks();
   Future<Either<AppFailure, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/feature/home/data/repo/home_repo_impl.dart
+++ lib/feature/home/data/repo/home_repo_impl.dart
@@ -1,21 +1,44 @@
 import 'package:bookly_app/core/error/errors.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 
 import 'package:bookly_app/feature/home/data/models/book_model/book_model.dart';
 
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 import 'home_repo.dart';
 
 class HomeServise implements HomeRepo {
+  ApiServise apiServise;
+  HomeServise(this.apiServise);
   @override
-  Future<Either<AppFailure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<AppFailure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServise.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science',
+      );
+      List<BookModel> books = [];
+      for (var element in data['items']) {
+        books.add(BookModel.fromJson(element));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 
   @override
   Future<Either<AppFailure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
 }
